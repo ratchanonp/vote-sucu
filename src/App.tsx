34c073268e48e725e422948bb86b81c420caa432
Ratchanon Panmas <qq.panmas@gmail.com>
2023-08-13 14:58:11 +0700

    💄 Placeholder Landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Container, Heading } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <></>
+    <Container height="100vw" display="flex" justifyContent="center" alignItems="center">
+      <Heading as="h1" size="4xl" textAlign="center">
+        มึงเร่งกุกุไม่ทำแม่ง อส
+      </Heading>
+    </Container>
   )
 }
 
