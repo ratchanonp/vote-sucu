2ed7b9931968024cdca83402e306c388c9dee946
Ratchanon Panmas <qq.panmas@gmail.com>
2023-08-13 15:16:42 +0700

    💄 Adjust Height

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Container, Heading } from "@chakra-ui/react"
 function App() {
 
   return (
-    <Container height="100vh" maxW="container.xl" display="flex" justifyContent="center" alignItems="center">
+    <Container height="100svh" maxW="container.xl" display="flex" justifyContent="center" alignItems="center">
       <Heading as="h1" size="4xl" textAlign="center">
-        มึงเร่งกุมาก กุลาออกแม่ง อส
+        ประชามติ
       </Heading>
     </Container>
   )
