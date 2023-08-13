ec397de31d48eb0ad2fb2ce48e6b1eeab273199c
Ratchanon Panmas <qq.panmas@gmail.com>
2023-08-13 15:09:57 +0700

    💄 Change font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Container, Heading } from "@chakra-ui/react"
 function App() {
 
   return (
-    <Container height="100vh" display="flex" justifyContent="center" alignItems="center">
+    <Container height="100vh" maxW="container.xl" display="flex" justifyContent="center" alignItems="center">
       <Heading as="h1" size="4xl" textAlign="center">
         มึงเร่งกุกุไม่ทำแม่ง อส
       </Heading>
