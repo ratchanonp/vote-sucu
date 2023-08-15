3609d68aecc514bbe95967f992ff90280e8d3b14
Ratchanon Panmas <qq.panmas@gmail.com>
2023-08-15 17:10:04 +0700

    Try Catch POST Request

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -10,17 +10,22 @@ const CallBack = () => {
     useEffect(() => {
         const getData = async (ticketId: string) => {
             const headers = new Headers();
+
             headers.append("DeeAppId", "app.web.vote-sucu")
             headers.append("DeeAppSecret", "05fa61aa574560830e5f460b33c55c377953d4142c2d39185b3f60c23d916dba45405e61fdc5b8a48338128e276aa0b9a4d5f1aaabb6274e0299dd8a42a9275c")
             headers.append("DeeTicket", ticketId)
 
-            const res = await fetch("https://account.it.chula.ac.th/serviceValidation", {
-                method: "POST",
-                headers,
-            });
+            try {
+                const res = await fetch("https://account.it.chula.ac.th/serviceValidation", {
+                    method: "POST",
+                    headers,
+                });
 
-            const data = await res.json();
-            console.log(data);
+                const data = await res.json();
+                console.log(data);
+            } catch (error) {
+                console.log(error);
+            }
         }
 
         if (ticketId) {
