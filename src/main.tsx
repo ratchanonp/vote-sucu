c042c210d3944396538658cf7048d7fcb9c2939a
Ratchanon Panmas <qq.panmas@gmail.com>
2023-08-23 18:51:13 +0700

    🚑  Fix Double render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
@@ -7,11 +6,9 @@ import { store } from './redux/store.ts'
 import { theme } from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ChakraProvider>
-  </React.StrictMode>,
+  <ChakraProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ChakraProvider>,
 )
